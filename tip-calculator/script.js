6e6467f233eb8ac46d728868def57bfe8f395718
yunusaltinyollar <yunusaltinyollar@gmail.com>
2025-10-04 23:36:42 +0300

    fix: tip calculator

--- tip-calculator/script.js
+++ tip-calculator/script.js
@@ -3,10 +3,9 @@ const btn1 = document.getElementById("btn");
 function calculate1(){
     const amountInput = document.getElementById("amount").value;
     const percentInput = document.getElementById("percent").value;
-    const result1 = (amountInput / 100) * percentInput;
-    const realResult = amountInput * percentInput;
+    const result1 = (amountInput / 100) * percentInput + Number(amountInput);
 
-    document.getElementById("result").innerText = (realResult);
+    document.getElementById("result").innerText = (result1);
 } 
 
     btn1.addEventListener("click", calculate1);

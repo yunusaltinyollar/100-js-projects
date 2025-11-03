95874edb50a9c3813ac5f8e03f9bf29d976014dd
yunusaltinyollar <yunusaltinyollar@gmail.com>
2025-11-03 23:42:36 +0300

    feat: add select menu

---
+++ select menu/script.js
@@ -0,0 +1,18 @@
+const selectField = document.getElementById("selectField");
+const selectText = document.getElementById("selectText");
+const options = document.getElementsByClassName("options");
+const list = document.getElementById("list");
+const arrowIcon = document.getElementById("arrowIcon");
+
+selectField.onclick = function(){
+    list.classList.toggle("hide");
+    arrowIcon.classList.toggle("rotate");
+}
+
+for(option of options){
+    option.onclick = function(){
+        selectText.innerHTML = this.textContent;
+        list.classList.toggle("hide");
+        arrowIcon.classList.toggle("rotate");
+    }
+}
